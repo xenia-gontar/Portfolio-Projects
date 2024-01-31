50dd159a519dd74cc77bd9fd8876a4f2ecaca8f0
Xenia Gontar <118064189+xenia-gontar@users.noreply.github.com>
2024-01-31 13:09:37 +0300

    Update exercise_3.py

--- test/exercise_3.py
+++ test/exercise_3.py
@@ -17,8 +17,6 @@ print(set(new_list) - (set(prime_sum_list)))
 
 available_sums = [11, 17, 23, 27, 29, 35, 37, 41, 47, 51, 53, 57, 59, 65, 67, 71, 77, 79, 83, 87, 89, 93, 95, 97]
 
-
-
 prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
 available_products = {}
@@ -37,7 +35,6 @@ for a in range(2,98):
 
 print(available_products)
 
-# удалить повторы:
 
 all_values = []
 
@@ -45,15 +42,10 @@ for value in available_products.values():
     for each_value in value:
         all_values.append(each_value)
 
-
-# посчитаем повторящиеся значения: 
-
 doubled_numbers = list(dict((x, all_values.count(x)) for x in set(all_values) if all_values.count(x) > 1).keys())
 
 
 for key, value in available_products.items():
-    print(f"Текущий ключ - {key}")
-    print(f"Текущее значение - {value}")
     list_of_num = value
     new_list = []
     for num in list_of_num:
@@ -61,7 +53,7 @@ for key, value in available_products.items():
             new_list.append(num)
     available_products[key] = new_list
 
-print(available_products)  
+print(available_products)    
 
 
         
